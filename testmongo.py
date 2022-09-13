44ffe5d51439acc3086b5b3b265a2094dba2d47a
Ajinkya <ajinkyanagare97@gmail.com>
2022-09-13 04:49:23 -0400

    this is a new test file

--- testmongo.py
+++ testmongo.py
@@ -1,19 +1,38 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Ajinkya:7028435990@cluster0.fxza7.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
 d = {
     "name":"Ajinkya",
     "email_id" : "ajinkya@gmail.com",
-"surname": "nagare"}d = {
-    "name":"Ajinkya",
-    "email_id" : "ajinkya@gmail.com",
-"surname": "nagare"}d = {
-    "name":"Ajinkya",
-    "email_id" : "ajinkya@gmail.com",
-"surname": "nagare"}d = {
-    "name":"Ajinkya",
-    "email_id" : "ajinkya@gmail.com",
-"surname": "nagare"}d = {
-    "name":"Ajinkya",
-    "email_id" : "ajinkya@gmail.com",
-"surname": "nagare"}d = {
+"surname": "nagare"}
+
+db1 = client["mongotest"]
+coll = db1['test']
+coll.insert_one(d)
+
+
+
+
+
+d = {
     "name":"Ajinkya",
     "email_id" : "ajinkya@gmail.com",
 "surname": "nagare"}
+d = {
+    "name":"Ajinkya",
+    "email_id" : "ajinkya@gmail.com",
+"surname": "nagare"}
+d = {
+    "name":"Ajinkya",
+    "email_id" : "ajinkya@gmail.com",
+"surname": "nagare"}
+d = {
+    "name":"Ajinkya",
+    "email_id" : "ajinkya@gmail.com",
+"surname": "nagare"}
+d = {
+    "name":"Ajinkya",
+    "email_id" : "ajinkya@gmail.com",
+"surname": "nagare"}
+
---
+++ testpy.py
@@ -0,0 +1,5 @@
+jfhsdfhsdsdbjvsdsdvsdv
+
+sdvsdvbsdbvdsbvj
+dvdsvbsdhvb
+nvbsdbvsdbv
