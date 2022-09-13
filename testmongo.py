fce731da278608d8b181d3784eaf244553bbe950
Ajinkya <ajinkyanagare97@gmail.com>
2022-09-13 04:46:27 -0400

    this is my first git push  with updation

--- testmongo.py
+++ testmongo.py
@@ -1,13 +1,19 @@
-import pymongo
-client = pymongo.MongoClient("mongodb+srv://Ajinkya:7028435990@cluster0.fxza7.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
-
 d = {
     "name":"Ajinkya",
     "email_id" : "ajinkya@gmail.com",
+"surname": "nagare"}d = {
+    "name":"Ajinkya",
+    "email_id" : "ajinkya@gmail.com",
+"surname": "nagare"}d = {
+    "name":"Ajinkya",
+    "email_id" : "ajinkya@gmail.com",
+"surname": "nagare"}d = {
+    "name":"Ajinkya",
+    "email_id" : "ajinkya@gmail.com",
+"surname": "nagare"}d = {
+    "name":"Ajinkya",
+    "email_id" : "ajinkya@gmail.com",
+"surname": "nagare"}d = {
+    "name":"Ajinkya",
+    "email_id" : "ajinkya@gmail.com",
 "surname": "nagare"}
-
-db1 = client["mongotest"]
-coll = db1['test']
-coll.insert_one(d)
-
